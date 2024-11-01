b8ded45456fa08ea8395e2d29c9b67d01586cc91
EzekielHall <hall.ei.2024@wvwc.edu>
2024-10-31 20:14:40 -0400

    created imageProcessing branch for working on images

---
+++ include/imageProcessing.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class ImageProcessing
+{
+    // TODO: Create a class that can take in an image and
+    //           A.) Resize an image.
+    //           B.) Convert the image to grayscale given x bits. For example on 4 bits for the "color".
+};
