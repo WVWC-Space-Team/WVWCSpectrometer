4bcd1bb428456f577971e98dc594afe7edbeb76e
Ezekiel (Zeke) Hall <hall.ei.2024@wvwc.edu>
2024-10-31 23:42:49 -0400

    updated notes for Yusuke

--- include/imageProcessing.h
+++ include/imageProcessing.h
@@ -3,6 +3,7 @@
 class ImageProcessing
 {
     // TODO: Create a class that can take in an image and
-    //           A.) Resize an image.
+    //           A.) Resize an image. Meaning you can pass a value x and y that the image will resize to
+    //               as long as those values do not exceed the size of the image.
     //           B.) Convert the image to grayscale given x bits. For example on 4 bits for the "color".
 };
