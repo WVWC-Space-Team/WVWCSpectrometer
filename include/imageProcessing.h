423e3b44db0f9886deece501118395b6f9a8ec81
Ezekiel (Zeke) Hall <hall.ei.2024@wvwc.edu>
2024-11-06 15:15:14 -0500

    made more specific comments

--- include/imageProcessing.h
+++ include/imageProcessing.h
@@ -5,5 +5,6 @@ class ImageProcessing
     // TODO: Create a class that can take in an image and
     //           A.) Resize an image. Meaning you can pass a value x and y that the image will resize to
     //               as long as those values do not exceed the size of the image.
-    //           B.) Convert the image to grayscale given x bits. For example on 4 bits for the "color".
+    //           B.) Convert the image to grayscale given "n" number of bits. For example, if passed 4 bits for the "color",
+    //               this will make it so that each "pixel" gets 4 bits for a brightness value.
 };
