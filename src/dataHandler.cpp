849811e4036e85cb2fc795a9ff14eb91c120701d
EzekielHall <hall.ei.2024@wvwc.edu>
2024-12-27 21:37:58 -0500

    made progress on compression alg

--- src/dataHandler.cpp
+++ src/dataHandler.cpp
@@ -4,17 +4,17 @@
 #include <vector>
 #include <cstdint>
 
-dataHandler::dataHandler() {}
+dataHandler::dataHandler(){}
 
-std::vector<uint16_t> dataHandler::compressRLE(const std::vector<uint16_t>& data)
+std::vector<uint16_t>* dataHandler::compressRLE(const std::vector<uint16_t>& data)
 {
-    std::vector<uint16_t> compressedData;
+    std::vector<uint16_t>* compressedData = new std::vector<uint16_t>();
     SONYILX511::MAX_INTENSITY_VALUE;
-    uint16_t count;
+    uint16_t count = 0;
     uint16_t value;
-    uint16_t previousValue;
+    uint16_t previousValue = 0;
     // NOTE: All these uint16 are technically only 12 bits due to having
-    // a 12 bit ADC.
+    // a 12 bit ADC built into the XIAO RP2040.
     for (const uint16_t& intensity : data)
     {
         if (intensity < SONYILX511::MAX_INTENSITY_VALUE * ZERO_DEADZONE)
@@ -24,8 +24,40 @@ std::vector<uint16_t> dataHandler::compressRLE(const std::vector<uint16_t>& data
         {
             value = intensity;
         }
-        count+= 1;
+        if (value > previousValue - COMPARE_DEADZONE && value < previousValue + COMPARE_DEADZONE)
+        {
+            count+= 1;
+        } else
+        {
+            if (count) // Takes care of instance where first value is 0
+            {
+                compressedData->push_back(value);
+                compressedData->push_back(count);
+                count = 0;
+            }
+        }
     };
 
     return compressedData;
 }
+
+bool dataHandler::checksumAmountOfSamples(std::vector<uint16_t>* compressedData)
+{
+    // Every second element is the count of times that value appears. The value should equal 2048.
+    // TODO: for every second element add value to count, if count 2048 return true else false
+}
+
+void dataHandler::updateZeroDeadzone(double newDeadzone)
+{
+    ZERO_DEADZONE = newDeadzone;
+};
+
+void dataHandler::updateCompareDeadzone(int newDeadzone)
+{
+    // NOTE: The compare deadzone is used to compare two values during the RLE compression algorithm.
+    // If the current value is within the range of "COMPARE_DEADZONE" away, the value is counted as the same.
+    // For example, if the current value is 600 and the previous was 590 and the deadzone is set to 30, the
+    // current value will be counted as the same ass the last one.
+    // TODO: Needs testing. Kind of seems like it might be easier to just reduce quality.
+    COMPARE_DEADZONE = newDeadzone;
+};
